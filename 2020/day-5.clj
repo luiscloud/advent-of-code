c1687b3dda0a015d4ffb60d4a03c86f79fa42038
luiscloud <30789757+luiscloud@users.noreply.github.com>
2021-11-20 15:18:44 +0800

    Delete 2020 directory

--- 2020/day-5.clj
+++
@@ -1,7 +0,0 @@
-(require '[clojure.string :as str])
-
-(defn tobin [dec] (loop [x (map #(Integer/parseInt %) (str/split dec #"")) b 0 exp 1] (if (empty? x) b (recur (butlast x) (+ b (* exp (last x))) (* 2 exp)))))
-
-(apply max (map #(+ (* 8 (tobin (subs % 0 7))) (tobin (subs % 7))) (map #(str/replace % #"[FL]" "0") (map #(str/replace % #"[BR]" "1") (str/split (slurp "day-5.txt") #"\r\n")))))
-
-(loop [seats (sort (map #(+ (* 8 (tobin (subs % 0 7))) (tobin (subs % 7))) (map #(str/replace % #"[FL]" "0") (map #(str/replace % #"[BR]" "1") (str/split (slurp "day-5.txt") #"\r\n")))))] (if (not (empty? seats)) (if (= 1 (- (first (rest seats)) (first seats))) (recur (rest seats)) (+ 1 (first seats)))))
